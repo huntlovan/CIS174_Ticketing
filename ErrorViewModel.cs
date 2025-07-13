901fb76092176a6e527c4788713f05c59e28bf15
huntlovan <hunter.lovan36@gmail.com>
2025-07-13 07:37:03 -0500

    clean up

--- ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CIS174_Ticketing.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# CIS174_Ticketing
