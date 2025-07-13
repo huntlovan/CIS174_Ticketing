1d74e3d73c92a17dec86984cd6da67599b5e9b96
huntlovan <hunter.lovan36@gmail.com>
2025-07-13 07:23:30 -0500

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CIS174_Ticketing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
