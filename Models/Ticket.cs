faf542e02be3f4e0a5b8be077fcc199333bb4d28
huntlovan <hunter.lovan36@gmail.com>
2025-07-20 11:22:52 -0500

    Update Ticket.cs

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -1,14 +1,20 @@
-﻿namespace CIS174_Ticketing.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace CIS174_Ticketing.Models
 {
     public class Ticket
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int TicketId { get; set; }
+        [Required(ErrorMessage = "Please enter a description.")]
         public string Description { get; set; }
-        public int SprintId { get; set; }
+        [Required(ErrorMessage = "Please select a sprint.")]
+        public string SprintId { get; set; } = string.Empty;
         public Sprint Sprint { get; set; }
+        [Required(ErrorMessage = "Please select a Point Value.")]
         public int PointValue { get; set; }
+        [Required(ErrorMessage = "Please enter a due date.")]
         public DateTime? DueDate { get; set; }
+        [Required(ErrorMessage = "Please select a status.")]
         public string StatusId { get; set; }
         public Status Status { get; set; }
         public bool Overdue =>
